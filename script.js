82c31b3b3c107a31422f7c7820606188692d55d2
Anushka Verma <anushkaverma779@gmail.com>
2023-02-16 16:04:03 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+
+let button = document.getElementById('button');
+
+
+button.addEventListener("click", function(e) {
+    e.preventDefault();
+    let name = document.getElementById('name').value;
+    let year = document.getElementById('year').value;
+    let h3 = document.getElementById("url");
+    if(name != "" && year!= "") {
+        h3.innerHTML = "https://localhost:8080/" + "?name=" + name + "&year=" + year;
+    }
+
+    if(name != "" && year == "") {
+        h3.innerHTML = "https://localhost:8080/" + "?name=" + name;
+    }
+
+    if(name == "" && year != "") {
+        h3.innerHTML = "https://localhost:8080/" + "?year=" + year;
+    }
+})
